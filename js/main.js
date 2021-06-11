7028a8efb367b8ef88af12dcc736147aee7c69d6
ca1r0n <egorfrolov2019.ef@gmail.com>
2021-06-11 17:17:40 +0300

    add gallery,about,map.offers,way

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
+"use strict";
+
 /**
   * название функции
   *
   * @param {number} first - первое число
   * @returns {number}
   */
-"use strict";
+$('.gallery__list').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  nextArrow: '.gallery__arrow--right',
+  prevArrow: '.gallery__arrow--left'
+});
 //# sourceMappingURL=main.js.map
