8522f465e318b9e55e6b811d95715781e03b0ccf
ca1r0n <egorfrolov2019.ef@gmail.com>
2021-06-17 19:42:55 +0300

    adaptation

--- js/main.js
+++ js/main.js
@@ -1,16 +1,42 @@
 "use strict";
 
-/**
-  * название функции
-  *
-  * @param {number} first - первое число
-  * @returns {number}
-  */
+var burger = document.querySelector('.burger');
+var burgerFooter = document.querySelector('.burger--footer');
+var width = document.documentElement.clientWidth;
+burger.addEventListener('click', function (e) {
+  e.currentTarget.classList.toggle('burger--active');
+});
+burgerFooter.addEventListener('click', function (e) {
+  e.currentTarget.classList.toggle('burger--active');
+});
 $('.gallery__list').slick({
   infinite: true,
   slidesToShow: 3,
   slidesToScroll: 1,
   nextArrow: '.gallery__arrow--right',
-  prevArrow: '.gallery__arrow--left'
+  prevArrow: '.gallery__arrow--left',
+  responsive: [{
+    breakpoint: 992,
+    settings: {
+      slidesToShow: 2
+    }
+  }, {
+    breakpoint: 480,
+    settings: {
+      slidesToShow: 1
+    }
+  }]
 });
+var offersList = document.querySelectorAll('.offers__items');
+var index = 0;
+
+if (width < 576) {
+  offersList.forEach(function (elem) {
+    if (index > 4 && index != offersList.length - 1) {
+      elem.classList.add('offers__items--no-active');
+    }
+
+    index++;
+  });
+}
 //# sourceMappingURL=main.js.map
